fe8f536331c8e8c2891fe21061876c832f5fbab0
polkabear <polkabear.git@gmail.com>
2015-02-21 00:00:12 -0500

    Initial commit

---
+++ app/src/main/java/com/clvsouthies/ax/androidtut4beginners/JSONAdaptor.java
@@ -0,0 +1,119 @@
+package com.clvsouthies.ax.androidtut4beginners;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by Annlee on 2/20/2015.
+ */
+public class JSONAdaptor extends BaseAdapter {
+    private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/";
+
+    Context mContext;
+    LayoutInflater mInflater;
+    JSONArray mJsonArray;
+
+    public JSONAdaptor(Context context, LayoutInflater inflater) {
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return mJsonArray.length();
+    }
+
+    @Override
+    public JSONObject getItem(int position) {
+        return mJsonArray.optJSONObject(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // your particular dataset uses StringIDs
+        // but you have to put something in this method
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        // check if the view already exists
+        // if so, no need to inflate and findViewById again!
+        if (convertView == null) {
+            // Inflate the custom row layout from your XML
+            convertView = mInflater.inflate(R.layout.row_book, null);
+
+            // create a new "Holder" with subviews
+            holder = new ViewHolder();
+            holder.thumbnailImageView = (ImageView) convertView.findViewById(R.id.img_thumbnail);
+            holder.titleTextView = (TextView) convertView.findViewById(R.id.text_title);
+            holder.authorTextView = (TextView) convertView.findViewById(R.id.text_author);
+
+            // hang on to this holder for future recylage
+            convertView.setTag(holder);
+        } else {
+            // skip all the expensive inflation/findViewById
+            // and just get the holder you already made
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // Get the current book's data in JSON form
+        JSONObject jsonObject = (JSONObject) getItem(position);
+
+        // See if there is a cover ID in the Object
+        if (jsonObject.has("cover_i")) {
+            // if so, grab the cover ID out from the object
+            String imageID = jsonObject.optString("cover_i");
+
+            // construct the image URL (specific to API)
+            String imageURL = IMAGE_URL_BASE + imageID + "-S.jpg";
+
+            // Use Picasso to load the image
+            // Temporarily have a placeholder in case it is slow to load
+
+            Picasso.with(mContext).load(imageURL).placeholder(R.drawable.ic_books)
+                    .into(holder.thumbnailImageView);
+        } else {
+            // if ther is no cover ID in the object, use a placeholder
+            holder.thumbnailImageView.setImageResource(R.drawable.ic_books);
+        }
+
+        // Grab the title and author from the JSON
+        String bookTitle = "";
+        String authorName = "";
+
+        if (jsonObject.has("title")) {
+            bookTitle = jsonObject.optString("title");
+        }
+
+        if (jsonObject.has("author_name")) {
+            authorName = jsonObject.optJSONArray("author_name").optString(0);
+        }
+
+        // Send these Strings to the TextViews for display
+        holder.titleTextView.setText(bookTitle);
+        holder.authorTextView.setText(authorName);
+
+        return convertView;
+    }
+
+    // this is used so you only ever have to do
+    // infaltion and finding by ID once ever per View
+    private static class ViewHolder {
+        public ImageView thumbnailImageView;
+        public TextView titleTextView;
+        public TextView authorTextView;
+    }
+}
